875afad732ee1a16eceb96054bd6e7fbde886a2c
taherabadi@gamelectronics.com <taherabadi@gamelectronics.com>
2023-11-28 19:44:40 +0330

    git init

---
+++ SampleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace SampleTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string word = "this the (test)";
+
+            var result = TextValidator.isValid(word);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleTest/TextValidator.cs
@@ -0,0 +1,44 @@
+﻿namespace SampleTest
+{
+    internal class TextValidator
+    {
+        public static bool isValid(string text)
+        {
+            int length = text.Length;
+            char[] charArray = text.ToCharArray();
+          
+            if (length == 0) 
+                return true;
+            
+            Stack<char> charactersStack = new Stack<char>();
+            for (int i = 0; i < length; i++)
+            {
+                if (charArray[i] is '(' or '{' or '[')
+                {
+                    charactersStack.Push(charArray[i]);
+                }
+                switch (charArray[i])
+                {
+                    case ')':  case '}': case ']':
+                        {
+                            if (charactersStack.Count == 0) 
+                                return false;
+
+                            char character = charactersStack.Pop();
+                            if ((character != '(' || charArray[i] != ')') && (character != '{' || charArray[i] != '}') && (character != '[' || charArray[i] != ']'))
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                }
+            }
+            if (charactersStack.Count != 0)
+                return false;
+            return true;
+        }
+    }
+}
